0d8ac29e23778d6d2bb27f6f75f5ee78964f7812
Ahmed Abdulmoniem <ahmed.abdulmoniem@hotmail.com>
2023-05-02 07:59:46 +0400

    Added AirQuality controller unit tests
    Signed-off-by: Ahmed Abdulmoniem <ahmed.abdulmoniem@hotmail.com>

--- src/air/controllers/air-quality/air-quality.controller.spec.ts
+++ src/air/controllers/air-quality/air-quality.controller.spec.ts
@@ -5,6 +5,7 @@ import { AirQualityController } from './air-quality.controller';
 
 describe('AirQualityController', () => {
   let controller: AirQualityController;
+  let service: AirQualityService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,9 +22,92 @@ describe('AirQualityController', () => {
     }).compile();
 
     controller = module.get<AirQualityController>(AirQualityController);
+    service = module.get<AirQualityService>(AirQualityService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('getAirQuality Method', () => {
+    it('should call getNearestCityByCoordinates method once from AirQualityService', () => {
+      const mock = jest
+        .spyOn(service, 'getNearestCityByCoordinates')
+        .mockImplementation(() => Promise.resolve({}));
+
+      controller.getAirQuality({ lat: 59.23, lng: 43.54 });
+
+      expect(mock).toBeCalledTimes(1);
+    });
+
+    it('should call getNearestCityByCoordinates method from AirQualityService with correct params', () => {
+      const mock = jest
+        .spyOn(service, 'getNearestCityByCoordinates')
+        .mockImplementation(() => Promise.resolve({}));
+
+      controller.getAirQuality({ lat: 59.23, lng: 43.54 });
+
+      expect(mock).toBeCalledWith(59.23, 43.54);
+    });
+
+    it('should return pollution information correctly', async () => {
+      const result = {
+        data: {
+          current: {
+            pollution: {
+              aqi: 10,
+              city: 'Paris',
+            },
+          },
+        },
+      };
+
+      const expected = {
+        Result: {
+          Pollution: {
+            aqi: 10,
+            city: 'Paris',
+          },
+        },
+      };
+
+      jest
+        .spyOn(service, 'getNearestCityByCoordinates')
+        .mockImplementation(() => Promise.resolve(result));
+
+      const actual = await controller.getAirQuality({ lat: 59.23, lng: 43.54 });
+
+      expect(actual).toStrictEqual(expected);
+    });
+  });
+
+  describe('getParisMostPollutedTime Method', () => {
+    it('should call getDateTimesWhenParisWasMostPolluted method once from AirQualityService', () => {
+      const mock = jest
+        .spyOn(service, 'getDateTimesWhenParisWasMostPolluted')
+        .mockImplementation(() => Promise.resolve(null));
+
+      controller.getParisMostPollutedTime();
+
+      expect(mock).toBeCalledTimes(1);
+    });
+
+    it('should return correct results from getDateTimesWhenParisWasMostPolluted method as is', async () => {
+      const expected = {
+        pollutionIndex: 79,
+        dateTimes: [
+          new Date('2023-05-01T08:09:01.000Z'),
+          new Date('2023-05-01T08:10:01.000Z'),
+        ],
+      };
+
+      jest
+        .spyOn(service, 'getDateTimesWhenParisWasMostPolluted')
+        .mockImplementation(() => Promise.resolve(expected));
+
+      const actual = await controller.getParisMostPollutedTime();
+
+      expect(actual).toStrictEqual(expected);
+    });
   });
 });
